bec1b7c9de8910fe89911464bedf98db431eda3f
Joel Santos <joe@joesantos.io>
2025-04-06 09:26:12 +0100

    implement calibrate

--- pkgcore/src/lib.rs
+++ pkgcore/src/lib.rs
@@ -1,6 +1,31 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
+pub fn calibrate(getter: fn() -> usize, max_count: usize, interval: usize) -> usize {
+    let mut curr_frame = 0;
+    let mut cached_count = 0;
+    let mut cache_sum = 0;
+
+    // loop and collect the necessary data for the calibration
+    while cached_count < max_count {
+        // wait for the interval to go through
+        if interval > curr_frame {
+            curr_frame += 1;
+            continue;
+        }
+
+        // reset the frame
+        curr_frame = 0;
+
+        // one more value cached
+        cache_sum += getter();
+        cached_count += 1;
+    }
+
+    // return the median
+    cache_sum / cached_count
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
