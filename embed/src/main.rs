5fea0a72b3f0a351eb717f8208a97018e7db43b1
Joel Santos <joe@joesantos.io>
2025-04-12 15:53:58 +0100

    tweak and improve usage of outputs

--- embed/src/main.rs
+++ embed/src/main.rs
@@ -11,7 +11,9 @@ use {defmt_rtt as _, panic_probe as _};
 // extern crate pkgcore;
 
 const FRAME_TIME: u64 = 100;
-const DEADZONE: f32 = 0.1;
+
+const CALIBRATE_DEADZONE: f32 = 0.1;
+const CALIBRATE_SAMPLES: usize = 16;
 const CALIBRATE_MIN: f32 = 0.0;
 
 #[embassy_executor::main]
@@ -31,28 +33,38 @@ async fn main(_spawner: Spawner) {
     let delay = Delay {};
     let mut load_sensor = hx711::HX711::new(hx711_sck, hx711_dt, delay);
     // zero the readings
-    load_sensor.tare(16);
+    load_sensor.tare(CALIBRATE_SAMPLES);
     load_sensor.set_scale(1.0);
 
     loop {
-        board_led.set_low();
-        output_a.set_low();
-        output_b.set_low();
-
-        // send output when endpoint is clicked
-        if endstop_input.is_high() {
-            board_led.set_high();
-            output_a.set_high();
-        }
+        let trigger_output_a = endstop_input.is_high();
+        let mut trigger_output_b = false;
 
         // wait for the load sensor
         if load_sensor.is_ready() {
             let reading = load_sensor.read_scaled().unwrap_or(0.0);
-            let is_over = reading - DEADZONE >= CALIBRATE_MIN;
-            if is_over {
-                board_led.set_high();
-                output_b.set_high();
-            }
+            trigger_output_b = reading - CALIBRATE_DEADZONE >= CALIBRATE_MIN;
+        }
+
+        // handle output a
+        if !trigger_output_a {
+            output_a.set_low();
+        } else {
+            output_a.set_high();
+        }
+
+        // handle output b
+        if !trigger_output_b {
+            output_b.set_low();
+        } else {
+            output_b.set_high();
+        }
+
+        // handle led
+        if trigger_output_a || trigger_output_b {
+            board_led.set_high();
+        } else {
+            board_led.set_low();
         }
 
         // wait for the frame time to go through
